e0cebe1d499d1bfffc6fb181ba0714c11fdb3464
p-o-l-e <ant.float.farm@gmail.com>
2026-03-25 08:00:31 +0300

    Update telescope.lua

--- lua/material/highlights/plugins/telescope.lua
+++ lua/material/highlights/plugins/telescope.lua
@@ -9,17 +9,17 @@ local M = {}
 M.load = function()
     local plugin_hls = {
         --TelescopeNormal         = { fg = e.fg, bg = b.floating_windows },
-        --TelescopePromptBorder   = { fg = e.border, bg = b.floating_windows },
-        --TelescopeResultsBorder  = { link = "TelescopePromptBorder" },
-        --TelescopePreviewBorder  = { link = "TelescopePromptBorder" },
-        --TelescopePromptTitle   = { fg = e.disabled, bg = b.floating_windows },
-        --TelescopeResultsTitle  = { link = "TelescopePromptTitle" },
-        --TelescopePreviewTitle  = { link = "TelescopePromptTitle" },
-        --TelescopePreviewDirectory = { fg = e.fg_dark },
-        --TelescopeSelectionCaret = { fg = e.title, bg = e.active },
-        --TelescopeSelection      = { fg = e.title, bg = e.active },
-        --TelescopeMultiSelection = { fg = m.yellow },
-        --TelescopeMatching       = { bold = true },
+        TelescopePromptBorder   = { fg = e.border, bg = b.floating_windows },
+        TelescopeResultsBorder  = { link = "TelescopePromptBorder" },
+        TelescopePreviewBorder  = { link = "TelescopePromptBorder" },
+        TelescopePromptTitle   = { fg = e.disabled, bg = b.floating_windows },
+        TelescopeResultsTitle  = { link = "TelescopePromptTitle" },
+        TelescopePreviewTitle  = { link = "TelescopePromptTitle" },
+        TelescopePreviewDirectory = { fg = e.fg_dark },
+        TelescopeSelectionCaret = { fg = e.title, bg = e.active },
+        TelescopeSelection      = { fg = e.title, bg = e.active },
+        TelescopeMultiSelection = { fg = m.yellow },
+        TelescopeMatching       = { bold = true },
     }
 
     return plugin_hls
